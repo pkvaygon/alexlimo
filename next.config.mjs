23fade90f181920386ed77531fd0e8cb45c21094
joelmiller1643@gmail.com <joelmiller1643@gmail.com>
2024-02-01 23:36:20 -0600

    add vehicles

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,23 @@ const nextConfig = {
     // output: 'export',
     // distDir: 'out',
     // trailingSlash: true,
+    reactStrictMode: true,
+    productionBrowserSourceMaps: true,
+    images: {
+        deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+      formats: ['image/avif', 'image/webp'],
+        unoptimized: true,
+        minimumCacheTTL: 60,
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'mail.google.com',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      },
         env: {
             SQUARE_APP_ID: "sq0idp-yUoS3szuMW-V0xJXrUVMQw",
             SQUARE_TOKEN: "EAAAFK_MhgmwTTDWuoPRjabTTP-aEKJAr-HjqMHR4V-p_7GscBdmX4SD0_s2OesS",
