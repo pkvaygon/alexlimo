9d19de86e2fe4c522ee3b1596578810c6b231af9
joelmiller1643@gmail.com <joelmiller1643@gmail.com>
2024-02-09 05:16:59 -0600

    video updated

--- components/BannerVideo.jsx
+++ components/BannerVideo.jsx
@@ -1,25 +1,24 @@
 "use client";
 import 'next-cloudinary/dist/cld-video-player.css';
-import getStarted from '@/public/chicagovideo.mp4';
 import { CldVideoPlayer } from 'next-cloudinary';
 export default function BannerVideo() {
-  
     return(
       <div className="w-full h-screen">
-        {/* <video className="w-full h-full object-cover brightness-50" controls={false} autoPlay loop muted preload="auto playsInline">
-        <source src="https://www.b-coleman.com/wp-content/uploads/2022/06/ver2-temp-skylinevid-home.mp4" type="video/mp4" />
-        </video> */}
-        <CldVideoPlayer
-        width="100%"
-          height="100%"
-        className="vjs-tech"
+        <video className="w-full h-full object-cover brightness-50" controls={false} autoPlay  muted preload="auto playsInline">
+          {/* <source src="https://firebasestorage.googleapis.com/v0/b/nextstore-a7837.appspot.com/o/localvideo.mp4?alt=media&token=030c4011-2d39-49ac-92b7-a273ba89ca54" type="video/mp4" /> */}
+          <source src="/localvideo.mp4" type="video/mp4" />
+        </video>
+        {/* <CldVideoPlayer
+        width="1000"
+          height="500"
+        className=" object-cover w-full h-full"
           controls={false}
           muted={true}
           autoplay
         sound={0}
         loop={true}
         src="https://res.cloudinary.com/dxvf93ovn/video/upload/v1706931839/chicagovideo_rjuqc6.mp4"
-        />
+        /> */}
       </div>
        )
   }
