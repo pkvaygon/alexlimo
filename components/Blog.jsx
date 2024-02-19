e0c1512940e2cc6b599a02f8d209f26de213906d
joelmiller1643@gmail.com <joelmiller1643@gmail.com>
2024-02-19 00:02:27 -0600

    new banner position

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -10,7 +10,7 @@ export default function Blog(){
     {
         image:'https://res.cloudinary.com/dxvf93ovn/image/upload/v1706915757/airtransport_fzsw07.jpg',
         label: 'Airport Transportation',
-        description: `Elite Luxury Limousine is the premier service to utilize for airport transportation service whether it’s to or from the airport. We serve
+        description: `We are is the premier service to utilize for airport transportation service whether it’s to or from the airport. We serve
         all major airports as well as all private jet centers. We offer world class airport car service our chauffeurs go above and beyond to
         provide the service you expect. We take great pride in making sure our clients enjoy first-class service and comfort as you travel
         in style to and from your destinations.`
@@ -18,7 +18,7 @@ export default function Blog(){
     {
         image:'https://res.cloudinary.com/dxvf93ovn/image/upload/v1708031489/corporate_meeting_ioons7.jpg',
         label: 'Corporate Serviсes',
-        description: `Elite Luxury Limousine is committed to providing our corporate limo service clientele with the best corporate transportation service
+        description: `We are is committed to providing our corporate limo service clientele with the best corporate transportation service
         available. Whether you’re entertaining existing or potential clients, company employees or just simply traveling to business
         meetings…Elite Luxury Limo has the vehicles and professionalism to serve all your corporate transportation needs. All of our
         chauffeurs are professionally trained drivers who are dedicated to making your business transportation service experience with
@@ -27,7 +27,7 @@ export default function Blog(){
     {
         image:'https://res.cloudinary.com/dxvf93ovn/image/upload/v1706915757/privatetour_badhzi.jpg',
         label: 'Private Excursion & Tours',
-        description: `Elite Luxury Limousine offers charter services in order to afford you a way to explore in luxury many well-known beautiful and
+        description: `We offer charter services in order to afford you a way to explore in luxury many well-known beautiful and
         exciting locations throughout Chicagoland. Our clients travel in the ultimate comfort driven by our professional chauffeurs that
         have in-depth knowledge of all landmarks and exciting destinations you’re seeking to visit. If you wish to visit somewhere off the
         beaten path it’s not an issue simply provide your chauffer with the details and address if available and they’ll be more than happy
@@ -36,7 +36,7 @@ export default function Blog(){
     {
         image:'https://res.cloudinary.com/dxvf93ovn/image/upload/v1708031549/events2_nkd62t.jpg',
         label: 'Events & Special Occasions',
-        description: `Elite Luxury Limousine specializes in providing you the highest level of professional transportation limousine services for any and all
+        description: `We specialize in providing you the highest level of professional transportation limousine services for any and all
         of your big moment and once-in-a-lifetime events and occasions. Let our professional chauffeurs drive you to that special
         moment in your life you’ll be driven in comfort unrushed and arrive on time.`,
     },
