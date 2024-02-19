2bd26bb55cdc63f51f0dc4ef8b802823595cf78a
joelmiller1643@gmail.com <joelmiller1643@gmail.com>
2024-02-19 01:06:54 -0600

    header title animation

---
+++ components/HeaderTitle.jsx
@@ -0,0 +1,36 @@
+"use client";
+import { motion } from 'framer-motion';
+
+export default function HeaderTitle(){
+
+    return (
+        <>
+               <motion.h1
+        initial={{ opacity: 0, y: 0, scale: 0.8 }}
+        animate={{ opacity: 1, y: 0, scale: 1 }}
+        transition={{
+          duration: 1.5,
+          ease: "easeInOut"
+        }}
+        className="text-8xl font-medium font-baskerville whitesd max-sm:text-3xl max-md:text-xl outline-1 outline-black outline-solid text-white "
+      >
+        Elite Luxury Limousine
+      </motion.h1>
+
+      <motion.h2
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 1.5,
+          ease: "easeInOut",
+          delay: 0.5
+        }}
+        className="text-4xl max-sm:text-sm max-md:text-md whitesd w-full font-medium font-baskerville text-white"
+      >
+        Experience isn't Expensive, it's Priceless
+      </motion.h2>
+
+        </>
+        )
+
+}
