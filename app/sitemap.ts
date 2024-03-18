1771847de54f46cb3ae8c0d4484552fb1db287da
joelmiller1643@gmail.com <joelmiller1643@gmail.com>
2024-03-17 20:08:31 -0500

    deploying

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,24 +1,42 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://acme.com',
+      url: "https://new.eliteluxurylimo.com/",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: 'https://acme.com/about',
+      url: "https://new.eliteluxurylimo.com/corporations",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://new.eliteluxurylimo.com/reservation",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
       priority: 0.9,
     },
     {
-      url: 'https://acme.com/blog',
+      url: "https://new.eliteluxurylimo.com/contact-us",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://new.eliteluxurylimo.com/privacy-policy",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
       priority: 0.5,
     },
-  ]
+    {
+      url: "https://new.eliteluxurylimo.com/terms-conditions",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
 }
